43a23d6427ee1d16a3d520962b5db4ccb8f2c04d
JayX97 <jason.fernandezx@yahoo.com>
2024-10-16 17:25:33 -0400

    Added functionality to operator buttons and additional code to number button handler function

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,32 +5,74 @@ const Calculator = (props) => {
     const operators = props.operators;
     const [input, setInput] = useState("0");
     const [output, setOutput] = useState("");
-    var result = 0;
+    var resultArr = []; // array used to collect numbers and operators for output display and expression calculation
 
     const handleNumInput = (event) => {// function for input display
         const value = event.target.value;
         const classList = event.target.classList;
 
         if (classList.contains("number")) {// if button is number
-            if (input === "0") setInput(value);
+            if (input === "0" || input === "/" || input === "X" || input === "+" || input === "-") setInput(value);
             else setInput(input + value);
-            setOutput(output + value);
+            
+            if (Number.isInteger(parseInt(output[output.length - 1])) || output[output.length - 1] === ".") setOutput(output + value);
+            else setOutput(output + " " + value);
         } else {
             switch(value) {
                 case "AC": // clear button
+                    resultArr = [];
                     setInput("0");
                     setOutput("");
+                    console.log(resultArr); // debugging
                     break;
                 case ".": // decimal button
-                    if (input.indexOf(".") === -1) { 
-                        setInput(input + value); //only add decimal point if no other decimal point exists in input string
-                        if (input.length === 1 && input[0] === "0") setOutput(input + value);
+                    if (input.indexOf(".") === -1) {// if no decimal point exists in the current input
+                        if (!(Number.isInteger(parseInt(input[0])))) setInput("0" + value); //change input display to 0. if current display is operator
+                        else setInput(input + value) //only add decimal point if no other decimal point exists in input string
+                        
+                        if (input.length === 1 && input[0] === "0") setOutput(input + value); //current input display is 0 and only 0
+                        else if (input.length === 1 && (!(Number.isInteger(parseInt(input[0]))))) setOutput(output + " 0" + value); //current input display is an operator
                         else setOutput(output + value);
                     }
                     break;
                 default:
                     break;
             }
+        }
+    };
+
+    const handleOperatorInput = (event) => {
+        const operator = event.target.value;
+        var outputAppend = output;
+
+        if (Number.isInteger(parseInt(output[output.length - 1]))) {
+            outputAppend = outputAppend + " ";
+        } else {
+            outputAppend = outputAppend.slice(0, -1);
+        }
+
+        switch(operator) {
+            case "/":
+                setInput("/");
+                setOutput(outputAppend + "/");
+                break;
+            case "X":
+                setInput("*");
+                setOutput(outputAppend + "*");
+                break;
+            case "+":
+                setInput("+");
+                setOutput(outputAppend + "+");
+                break;
+            case "-":
+                setInput("-");
+                setOutput(outputAppend + "-");
+                break;
+            case "=":
+                console.log(resultArr); // debugging
+                break;
+            default:
+                break;
         }
     };
 
@@ -52,7 +94,7 @@ const Calculator = (props) => {
                     {
                         operators.map(operator => {
                             return (
-                                <button className={operator.classes} id={operator.id}>{operator.label}</button>
+                                <button onClick={handleOperatorInput} className={operator.classes} id={operator.id} value={operator.label}>{operator.label}</button>
                             )
                         })
                     }
